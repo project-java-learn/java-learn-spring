fdc513059fdd812fe95f3521c9b8cec61db48e55
Matthew Morgan <mmorgan55@cnm.edu>
2018-12-07 11:50:25 -0700

    Added Google Authentication.

--- src/main/java/edu/cnm/deepdive/javalearn/JavalearnApplication.java
+++ src/main/java/edu/cnm/deepdive/javalearn/JavalearnApplication.java
@@ -1,12 +1,37 @@
 package edu.cnm.deepdive.javalearn;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
+@EnableWebSecurity
+@EnableResourceServer
 @SpringBootApplication
-public class JavalearnApplication {
+public class JavalearnApplication extends ResourceServerConfigurerAdapter {
+
+  //TODO Make this extend ResourceConfigurerADapter
+
+  @Value("${oauth.clientId}")
+  private String clientId;
 
   public static void main(String[] args) {
     SpringApplication.run(JavalearnApplication.class, args);
+  }
+
+  @Override
+  public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+    resources.resourceId(clientId);
+  }
+
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http.authorizeRequests().anyRequest().hasRole("USER");
   }
 }
